ad9c6b395f7e5ce0d534c93ffb644d1a799da9a9
Hayden Jones <122436575+HaydenRJones@users.noreply.github.com>
2025-09-15 20:50:33 +1200

    Update stripSeqs.py

--- python/stripSeqs.py
+++ python/stripSeqs.py
@@ -1,64 +1,53 @@
 # stripSeqs.py
 # parse meshclust3 output into induvidual fasta files
-# HJ - 31/10/23
+# HJ - 15/09/25
 
+import sys
 import pandas as pd
 from Bio import SeqIO
 
-def stripSeqs(barcode, clusterDist)
+seq_file   = sys.argv[1]
+clust_file = sys.argv[2]
 
-        colnames = ['cluster', 'index', 'identity', 'score']
-        clusters = pd.read_csv(f'./clusters/{clusterDist}_{barcodes}.txt', sep = '\t', header = None, names = colnames)
-        clusters = clusters[clusters.groupby('cluster')['cluster'].transform('size') > 1]
-        
-        with open(f'./aligned/{barcode}.fas') as fasta_file:
-            identifiers = []
-            sequences = []
-            for record in SeqIO.parse(fasta_file, 'fasta'):
-                identifiers.append(record.id)
-                sequences.append(str(record.seq))
-     
-        sequences = {'index': identifiers, 'sequence': sequences}
-        sequences  = pd.DataFrame(sequences)
-        
-        for x in range(1, clusters['cluster'].nunique() + 1):
-                
-            clusterSeqs = pd.DataFrame(columns = ['index', 'sequence'])
-        
-            workingData = clusters.loc[clusters['cluster'] == x]
-        
-            for j in range(0, len(workingData)):
-                target = workingData['index'].iloc[j].strip('>')       
-                seq = pd.DataFrame(sequences.loc[sequences['index'] == target])
-        
-                if not seq.empty:
-                    
-                    clusterSeqs = pd.concat([clusterSeqs, seq], ignore_index = True , keys=['index', 'sequence'])
-        
-                else:
-                    print(f'Index \'{target}\' not found in sequences DataFrame')
-                    
-                
-                f = open(f'./clusters/{barcode}_{x}.fasta', 'w')
-                
-                for k in range(0, len(clusterSeqs)):
-                    
-                    name = clusterSeqs['index'].iloc[k]
-                    seq = clusterSeqs['sequence'].iloc[k]
-                    
-                    f.write(f'>{name}\n{seq}\n')
-                    
-                f.close()
-                
-            print(f'Clusters stripped for {barcode}, cluster {x}')
-    
-    return()        
+colnames = ['cluster', 'index', 'identity', 'score']
+clusters = pd.read_csv(clust_file, sep = '\t', header = None, names = colnames)
+clusters = clusters[clusters.groupby('cluster')['cluster'].transform('size') > 1]
 
-#########
+with open(seq_file) as fasta_file:
+    identifiers = []
+    sequences = []
+    for record in SeqIO.parse(fasta_file, 'fasta'):
+        identifiers.append(record.id)
+        sequences.append(str(record.seq))
+ 
+sequences = {'index': identifiers, 'sequence': sequences}
+sequences  = pd.DataFrame(sequences)
 
-barcodes = []
-clusterDist = 99
+for x in range(1, clusters['cluster'].nunique() + 1):
+        
+    clusterSeqs = pd.DataFrame(columns = ['index', 'sequence'])
 
-for i in range(0,len(barcodes))
-    
-    stripSeqs(barcodes[i], clusterDist)
+    workingData = clusters.loc[clusters['cluster'] == x]
+
+    for j in range(0, len(workingData)):
+        target = workingData['index'].iloc[j].strip('>')       
+        seq = pd.DataFrame(sequences.loc[sequences['index'] == target])
+
+        if not seq.empty:
+            
+            clusterSeqs = pd.concat([clusterSeqs, seq], ignore_index = True , keys=['index', 'sequence'])
+
+        else:
+            print(f'Index \'{target}\' not found in sequences DataFrame')
+            
+        
+        f = open(f"{seq_file}_{x}", 'w')
+        
+        for k in range(0, len(clusterSeqs)):
+            
+            name = clusterSeqs['index'].iloc[k]
+            seq = clusterSeqs['sequence'].iloc[k]
+            
+            f.write(f'>{name}\n{seq}\n')
+            
+        f.close()
